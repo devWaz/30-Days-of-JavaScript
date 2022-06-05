1336acb594eb4d44c4ae4fd93bf74fc4cdd138e1
devWaz <fawazodessy@gmail.com>
2022-06-05 15:40:22 +0100

    Day 1 Complete

--- Day 1 - Drum Kit/script.js
+++ Day 1 - Drum Kit/script.js
@@ -1,8 +1,20 @@
-window.addEventListener('keydown' , (e) => {
+window.addEventListener('keydown' ,playSound );
+
+function playSound (e) {
     const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if (!sound) return;
-    sound.currentTime = 0;
-    sound.play();
-    key.classList.add("playing")
-});
+    if (!sound) return; 
+    sound.currentTime = 0; //sets the audio to the start so it can be hit multiple times
+    sound.play(); //plays the audio
+    key.classList.add("playing"); //adds transition and border
+}
+
+function removeTransition(e){
+    if (e.propertyName !== 'transform') return; //skip it if it's not transform
+    e.target.classList.remove('playing'); //removes the class 'playing'
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend' , removeTransition)); //removes the 
+// transform property after the transition ends
+
