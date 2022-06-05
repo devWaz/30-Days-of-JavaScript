fc991fba73a4cc2bed2c2c86bb2d57901c7b21dd
devWaz <fawazodessy@gmail.com>
2022-06-05 12:05:11 +0100

    Sound Updates

--- Day 1 - Drum Kit/script.js
+++ Day 1 - Drum Kit/script.js
@@ -1,4 +1,8 @@
 window.addEventListener('keydown' , (e) => {
     const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    sound.play()
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!sound) return;
+    sound.currentTime = 0;
+    sound.play();
+    key.classList.add("playing")
 });
