e1a98d7d5d83b80ba9b84b9b81bc668d9ab11f04
devWaz <fawazodessy@gmail.com>
2022-06-06 23:40:21 +0100

    Script

---
+++ Day 2 - JS & CSS Clock/script.js
@@ -0,0 +1,9 @@
+const secondsHand = document.querySelector('.seconds-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function theTime(){
+    let timeNow = new Date();
+
+    
+}
